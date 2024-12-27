2edd4f786f117324aca5c6a82b10f0d79a210b22
rohitmangal4 <mangaleswaran2001@gmail.com>
2024-12-27 05:38:16 +0530

    worked alsomsr

--- src/Components/CheckAppointment.jsx/CheckOfflineAppointment/CheckOfflinemain.jsx
+++ src/Components/CheckAppointment.jsx/CheckOfflineAppointment/CheckOfflinemain.jsx
@@ -33,7 +33,7 @@ const CheckOfflineMain = () => {
 
   return (
     <div className="flex">
-      <div className="bg-gray-200 w-full h-screen p-7">
+      <div className="bg-gray-200 w-full min-h-screen p-7">
         {/* Display loader while data is loading */}
         {loading ? (
           <Loader />
